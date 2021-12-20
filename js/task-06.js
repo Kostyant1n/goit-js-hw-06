1fbaf7d553682f22224773991799898237201aaa
Smilyk Kostiantyn <bmd.Warher@gmail.com>
2021-12-20 09:26:13 +0200

    +

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("#validation-input");
 const artLength = form.getAttribute("data-length");
 form.onblur = function () {
-  if (artLength == this.value.length) {
+  if (artLength.includes(this.value.length)) {
     this.classList.remove("invalid");
     this.classList.add("valid");
     return;
